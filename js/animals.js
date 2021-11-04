ff7666975b2ab4148eb79c344f83287d5a0df498
helpMeRise <rises128@gmail.com>
2021-11-04 20:05:05 +0200

    адаптировал хедер и первую секцию

--- js/animals.js
+++ js/animals.js
@@ -13,27 +13,27 @@ export const animals = () => {
       if (target.textContent == 'Борис') {
         animal.setAttribute('src', 'image/hero/boris_big.png')
         animalName.textContent = 'Борис'
-        who.textContent = 'его'
+        who.innerHTML = `его`
         remove('Борис');
       } else if (target.textContent == 'Луи') {
         animal.setAttribute('src', 'image/hero/lui_big.png')
         animalName.textContent = 'Луи'
-        who.textContent = 'его'
+        who.innerHTML = `его`
         remove('Луи');
       } else if (target.textContent == 'Питер') {
         animal.setAttribute('src', 'image/hero/piter_big.png')
         animalName.textContent = 'Питер'
-        who.textContent = 'его'
+        who.innerHTML = `его`
         remove('Питер');
       } else if (target.textContent == 'Кенни') {
         animal.setAttribute('src', 'image/hero/kenni_big.png')
         animalName.textContent = 'Кенни'
-        who.textContent = 'его'
+        who.innerHTML = `его`
         remove('Кенни');
       } else if (target.textContent == 'Сара') {
         animal.setAttribute('src', 'image/hero/sara_big.png')
         animalName.textContent = 'Сара'
-        who.textContent = 'её'
+        who.innerHTML = `её`
         remove('Сара');
       }
     })
