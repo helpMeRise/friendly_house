0f14a91adc81de1a7a5cb0ad6d33e47950be6771
helpMeRise <rises128@gmail.com>
2021-11-05 23:10:42 +0200

    Написал разметку для секции наши питомцы

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
+import sassPkg from 'sass';
+import gulpSass from 'gulp-sass';
 import gulpCssimport from 'gulp-cssimport';
 import del from 'del';
+
+
+const prepros = true;
+
+const sass = gulpSass(sassPkg);
 
 // Задачи
 
@@ -10,20 +17,29 @@ export const html = () => gulp
   .pipe(gulp.dest('dist'))
   .pipe(browserSync.stream());
 
-export const css = () => gulp
+export const style = () => {
+  if (prepros) {
+    return gulp
+    .src('./src/scss/**.*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(browserSync.stream())
+  }
+
+  return gulp
   .src('src/css/index.css')
   .pipe(gulpCssimport({
     extensions: ['css'],
   }))
   .pipe(gulp.dest('dist/css'))
   .pipe(browserSync.stream());
+}
 
 export const js = () => gulp
   .src('src/js/**/*.js')
   .pipe(gulp.dest('dist/js'))
   .pipe(browserSync.stream());
 
-export const copy = (done) => { gulp
+export const copy = () => gulp
   .src([
     'src/fonts/**/*',
     'src/image/**/*'
@@ -33,9 +49,8 @@ export const copy = (done) => { gulp
   .pipe(gulp.dest('dist'))
   .pipe(browserSync.stream({
     once: true
-  }))
-  done();
-}
+}))
+
 
 export const server = () => {
   browserSync.init({
@@ -48,7 +63,7 @@ export const server = () => {
   })
 
   gulp.watch('./src/**/*.html', html);
-  gulp.watch('./src/css/**/*.css', css);
+  gulp.watch(prepros ? './src/scss/**/*.scss' : './src/css/**/*.css', style);
   gulp.watch('./src/js/**/*.js', js);
   gulp.watch(['./src/image/**/*', './src/fonts/**/*'], copy);
 }
@@ -57,7 +72,7 @@ export const clear = () => del('dist/**/*', {forse: true});
 
 //Запуск
 
-export const base = gulp.parallel(html, css, js, copy);
+export const base = gulp.parallel(html, style, js, copy);
 
 export const build = gulp.series(clear, base)
 
