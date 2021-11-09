293da3fe8427f1c158ee99cd340d3415c0272233
helpMeRise <rises128@gmail.com>
2021-11-09 12:26:06 +0200

    настрол галп для кроссбраузерности

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ import gulpWebp from 'gulp-webp';
 import gulpAvif from 'gulp-avif';
 import { stream as critical } from 'critical';
 import gulpif from 'gulp-if';
+import autoprefixer from 'gulp-autoprefixer';
+import babel from 'gulp-babel';
 
 const prepros = true;
 
@@ -23,7 +25,6 @@ const sass = gulpSass(sassPkg);
 
 const allJS = [
   "src/libs/jquery-3.6.0.min.js",
-  "src/js/index.js",
 ];
 
 // Задачи
@@ -44,6 +45,7 @@ export const style = () => {
       .src('src/scss/**/*.scss')
       .pipe(gulpif(dev, sourcemaps.init()))
       .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
       .pipe(cleanCSS({
         2: {
           specialComments: 0,
@@ -60,6 +62,7 @@ export const style = () => {
     .pipe(gulpCssimport({
       extensions: ['css'],
     }))
+    .pipe(autoprefixer())
     .pipe(cleanCSS({
       2: {
         specialComments: 0,
@@ -73,6 +76,10 @@ export const style = () => {
 export const js = () => gulp
   .src([...allJS, 'src/js/**/*.js'])
   .pipe(gulpif(dev, sourcemaps.init()))
+  .pipe(babel({
+    presets: ['@babel/preset-env'],
+    ignore: [...allJS]
+  }))
   .pipe(terser())
   .pipe(concat("index.min.js"))
   .pipe(sourcemaps.write('../maps'))
